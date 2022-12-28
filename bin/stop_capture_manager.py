7cb9269cce6602431f45a72a8d3c96fdb8b69036
Raphaël Vinot <raphael@vinot.info>
2022-12-28 12:44:00 +0100

    new: Script to stop capture_manager only

---
+++ bin/stop_capture_manager.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import logging.config
+
+import psutil
+import signal
+
+from lacus.default import get_config
+
+logging.config.dictConfig(get_config('logging'))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Sends a SIGTERM to the capture_manager so you can restart it cleanly.')
+    parser.parse_args()
+
+    found = False
+    for p in psutil.process_iter(['name']):
+        if p.info['name'] == "capture_manager":
+            p.send_signal(signal.SIGTERM)
+            found = True
+
+    if not found:
+        print('Unable to find capture_manager')
+
+
+if __name__ == '__main__':
+    main()
