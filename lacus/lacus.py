edf6ac9ae2aace010cc8083815007df9b704fe62
Raphaël Vinot <raphael@vinot.info>
2023-05-25 15:27:55 +0200

    chg: Improve error handling, attempt to limit redis connection errors

--- lacus/lacus.py
+++ lacus/lacus.py
@@ -15,8 +15,8 @@ class Lacus():
         self.logger = logging.getLogger(f'{self.__class__.__name__}')
         self.logger.setLevel(get_config('generic', 'loglevel'))
 
-        self.redis = Redis(unix_socket_path=get_socket_path('cache'))
-        self.redis_decode = Redis(unix_socket_path=get_socket_path('cache'), decode_responses=True)
+        self.redis = Redis(unix_socket_path=get_socket_path('cache'), health_check_interval=30)
+        self.redis_decode = Redis(unix_socket_path=get_socket_path('cache'), decode_responses=True, health_check_interval=30)
 
         self.core = LacusCore(self.redis, tor_proxy=get_config('generic', 'tor_proxy'),
                               only_global_lookups=get_config('generic', 'only_global_lookups'),
