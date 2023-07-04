0178403c27cec0aabb79f5bb0fa121d0a8ac4cb0
lcorrea <lcorrea@florestleaks.com.br>
2023-07-04 16:39:31 -0400

    docs(website/lacus.default): documentation

--- website/web/helpers.py
+++ website/web/helpers.py
@@ -6,6 +6,16 @@ from lacus.default import get_homedir
 
 
 def src_request_ip(request) -> str:
+    """
+    Get the source IP address from the request object.
+
+    Args:
+        request: The request object containing the headers and remote address.
+
+    Returns
+    -------
+        The source IP address as a string.
+    """
     # NOTE: X-Real-IP is the IP passed by the reverse proxy in the headers.
     real_ip = request.headers.get("X-Real-IP")
     if not real_ip:
@@ -15,6 +25,13 @@ def src_request_ip(request) -> str:
 
 @lru_cache(64)
 def get_secret_key() -> bytes:
+    """
+    Get the secret key used for encryption.
+
+    Returns
+    -------
+        The secret key as bytes.
+    """
     secret_file_path: Path = get_homedir() / "secret_key"
     if not secret_file_path.exists() or secret_file_path.stat().st_size < 64:
         if not secret_file_path.exists() or secret_file_path.stat().st_size < 64:
