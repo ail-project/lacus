91e5e5fba19be90fe8bab6f43d8e81984c37fac3
Raphaël Vinot <raphael@vinot.info>
2023-08-09 14:30:24 +0200

    new: Optional favicon fetching
    Ref https://github.com/Lookyloo/PlaywrightCapture/issues/45

--- website/web/__init__.py
+++ website/web/__init__.py
@@ -33,44 +33,45 @@ class RedisUp(Resource):
 
 
 http_creds_model = api.model('HttpCredentialtModel', {
-    'username': fields.String(),
-    'password': fields.String()
+    'username': fields.String(example='admin'),
+    'password': fields.String(example='password')
 })
 
 viewport_model = api.model('ViewportModel', {
-    'width': fields.Integer(),
-    'height': fields.Integer()
+    'width': fields.Integer(example=1024),
+    'height': fields.Integer(example=768)
 })
 
 geolocation_model = api.model('GeolocalisationModel', {
-    'longitude': fields.Float(),
-    'latitude': fields.Float()
+    'longitude': fields.Float(example=12.453389),
+    'latitude': fields.Float(example=41.902916)
 })
 
 
 submit_fields_post = api.model('SubmitFieldsPost', {
-    'url': fields.Url(description="The URL to capture"),
-    'document': fields.String(description="A base64 encoded document, it can be anything a browser can display."),
-    'document_name': fields.String(description="The name of the document."),
-    'depth': fields.Integer(description="Depth of the capture, based on te URLs that can be found in the rendered page."),
-    'rendered_hostname_only': fields.Boolean(description="If depth is >0, which URLs are we capturing (only the ones with the same hostname as the rendered page, or all of them.)"),
-    'browser': fields.String(description="Use this browser. Must be chromium, firefox or webkit.", example=''),
-    'device_name': fields.String(description="Use the pre-configured settings for this device. Get a list from /json/devices.", example=''),
-    'user_agent': fields.String(description="User agent to use for the capture", example=''),
+    'url': fields.Url(description="The URL to capture", example=''),
+    'document': fields.String(description="A base64 encoded document, it can be anything a browser can display.", example=''),
+    'document_name': fields.String(description="The name of the document.", example=''),
+    'depth': fields.Integer(description="Depth of the capture, based on te URLs that can be found in the rendered page.", example=1),
+    'rendered_hostname_only': fields.Boolean(description="If depth is >0, which URLs are we capturing (only the ones with the same hostname as the rendered page, or all of them.)", example=True),
+    'browser': fields.String(description="Use this browser. Must be chromium, firefox or webkit.", example='webkit'),
+    'device_name': fields.String(description="Use the pre-configured settings for this device. Get a list from /json/devices.", example='Nexus 6'),
+    'user_agent': fields.String(description="User agent to use for the capture", example='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0'),
     'proxy': fields.Url(description="Proxy to use for the capture. Format: [scheme]://[username]:[password]@[hostname]:[port]", example=''),
-    'general_timeout_in_sec': fields.Integer(description="General timeout for the capture. It will be killed regardless the status after that time."),
+    'general_timeout_in_sec': fields.Integer(description="General timeout for the capture. It will be killed regardless the status after that time.", example=300),
     'cookies': fields.String(description="JSON export of a list of cookies as exported from an other capture", example=''),
     'headers': fields.String(description="Headers to pass to the capture", example='Accept-Language: en-US;q=0.5, fr-FR;q=0.4'),
     'http_credentials': fields.Nested(http_creds_model, description="HTTP Authentication settings"),
     'geolocation': fields.Nested(geolocation_model, description="The geolocalisation of the browser"),
     'viewport': fields.Nested(viewport_model, description="The viewport of the capture"),
-    'timezone_id': fields.String(description="The timesone ID of the browser"),
-    'locale': fields.String(description="The locale of the browser"),
-    'color_scheme': fields.String(description="The color scheme of the browser"),
-    'referer': fields.String(description="Referer to pass to the capture", example=''),
-    'force': fields.Boolean(description="Force a capture, even if the same one was already done recently"),
-    'recapture_interval': fields.Integer(description="The nimomal interval to re-trigger a capture, unless force is True"),
-    'priority': fields.Integer(description="Priority of the capture, the highest, the better"),
+    'timezone_id': fields.String(description="The timezone ID of the browser", example='Europe/Paris'),
+    'locale': fields.String(description="The locale of the browser", example='en-US'),
+    'color_scheme': fields.String(description="The color scheme of the browser", example='dark'),
+    'referer': fields.String(description="Referer to pass to the capture", example='https://circl.lu'),
+    'with_favicon': fields.Boolean(description="Attempts to get favicons related to the landing page of the capture", example=False),
+    'force': fields.Boolean(description="Force a capture, even if the same one was already done recently", example=False),
+    'recapture_interval': fields.Integer(description="The minimal interval to re-trigger a capture, unless force is True", example=300),
+    'priority': fields.Integer(description="Priority of the capture, the highest, the better", example=-5),
 })
 
 
@@ -100,6 +101,7 @@ class Enqueue(Resource):
             locale=to_query.get('locale'),
             color_scheme=to_query.get('color_scheme'),
             referer=to_query.get('referer'),
+            with_favicon=to_query.get('with_favicon', False),
             rendered_hostname_only=to_query.get('rendered_hostname_only', True),
             force=to_query.get('force', False),
             recapture_interval=to_query.get('recapture_interval', 300),
