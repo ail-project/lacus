92b16cdef1b757ae10438072e763b769770b4910
Raphaël Vinot <raphael@vinot.info>
2024-08-07 11:54:41 +0200

    new: controller to list/stop/restart scripts indiviually

--- bin/capture_manager.py
+++ bin/capture_manager.py
@@ -47,28 +47,29 @@ class CaptureManager(AbstractManager):
                     self.logger.warning(f'{expected_uuid} is canceled now.')
 
     async def _to_run_forever_async(self) -> None:
+
+        def clear_list_callback(task: Task) -> None:  # type: ignore[type-arg]
+            self.captures.discard(task)
+            self.unset_running()
+
         await self.clear_dead_captures()
         if self.force_stop:
             return
         max_new_captures = get_config('generic', 'concurrent_captures') - len(self.captures)
-        self.logger.debug(f'{len(self.captures)} ongoing captures.')
         if max_new_captures <= 0:
             if len(self.lacus.monitoring.get_enqueued_captures()) > 0:
                 self.logger.debug(f'Max amount of captures in parallel reached ({len(self.captures)})')
             return
         for capture_task in self.lacus.core.consume_queue(max_new_captures):
             self.captures.add(capture_task)
-            capture_task.add_done_callback(self.captures.discard)
-        # NOTE: +1 because running this method also counts for one and will
-        #       be decremented when it finishes
-        self.set_running(len(self.captures) + 1)
+            self.set_running()
+            capture_task.add_done_callback(clear_list_callback)
 
     async def _wait_to_finish_async(self) -> None:
         while self.captures:
             self.logger.info(f'Waiting for {len(self.captures)} capture(s) to finish...')
             self.logger.info(f'Ongoing captures: {", ".join(capture.get_name() for capture in self.captures)}')
             await asyncio.sleep(5)
-            self.set_running(len(self.captures))
         self.logger.info('No more captures')
 
 
