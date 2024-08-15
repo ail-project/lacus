7a881aa7d850be94c383dc6197e88dd69556e050
Raphaël Vinot <raphael@vinot.info>
2024-08-15 11:55:25 +0200

    chg: Bump gunicorn

--- bin/start_website.py
+++ bin/start_website.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
 import logging
 import logging.config
 
-from typing import Optional
-
 from subprocess import Popen
 
-from lacus.default import AbstractManager
-from lacus.default import get_config, get_homedir
+from lacus.default import get_config, get_homedir, AbstractManager
 
 logging.config.dictConfig(get_config('logging'))
 
 
 class Website(AbstractManager):
 
-    def __init__(self, loglevel: Optional[int]=None) -> None:
+    def __init__(self, loglevel: int | None=None) -> None:
         super().__init__(loglevel)
         self.script_name = 'website'
-        self.process = self._launch_website()
+        self.process: Popen = self._launch_website()  # type: ignore[type-arg]
         self.set_running()
 
     def _launch_website(self) -> Popen:  # type: ignore[type-arg]
@@ -29,6 +28,9 @@ class Website(AbstractManager):
                       '--graceful-timeout', '2', '--timeout', '300',
                       '-b', f'{ip}:{port}',
                       '--log-level', 'info',
+                      '--max-requests', '2000',
+                      '--max-requests-jitter', '100',
+                      '--name', 'website_lacus',
                       'web:app'],
                      cwd=website_dir)
 
