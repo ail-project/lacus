eb5d835c04d75fc032fccaa03e2af342411fde3f
Raphaël Vinot <raphael@vinot.info>
2025-05-14 21:23:22 +0200

    fix: Set timeout for port check in wireproxy manager

--- bin/wireproxy_manager.py
+++ bin/wireproxy_manager.py
@@ -88,6 +88,7 @@ class WireProxyFSManager(PatternMatchingEventHandler):
     def _port_in_use(self, port: int) -> bool:
         """Check if the port in currently in use on the machine."""
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(1)
             return s.connect_ex(('127.0.0.1', port)) == 0
 
     def _init_configs(self) -> None:
