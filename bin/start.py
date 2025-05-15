30c6f06ebf90bc8198d3afeafbc1fe982a6918ce
Raphaël Vinot <raphael@vinot.info>
2025-05-15 15:44:11 +0200

    new: Add wireproxy_manager in autostart

--- bin/start.py
+++ bin/start.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 from subprocess import Popen, run
 
-from lacus.default import get_homedir
+from lacus.default import get_homedir, get_config
 
 
 def main() -> None:
@@ -18,6 +19,17 @@ def main() -> None:
     print('Start Capture manager...')
     Popen(['capture_manager'])
     print('done.')
+
+    # Is configured, start wireproxies
+    if path_to_wireproxy := Path(get_config('generic', 'wireproxy_path')):
+        if path_to_wireproxy.exists() and path_to_wireproxy.is_file():
+            print('Start wireproxy...')
+            Popen(['wireproxy_manager'])
+            print('done.')
+        else:
+            print('Wireproxy executable missing, skipping.')
+    else:
+        print('Wireproxy not configured, skipping.')
 
 
 if __name__ == '__main__':
